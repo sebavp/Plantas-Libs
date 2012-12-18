c4a19157656fa5f4b6c7db322ded5cf18bc88dd7
Sebastian Valenzuela <sebastiandfg@gmail.com>
2012-12-18 16:53:44 -0300

    EFD

--- plants_libs/EFD.py
+++ plants_libs/EFD.py
@@ -4,8 +4,8 @@ from sys import argv
 
 from PIL.ImageFilter import CONTOUR, SHARPEN, EDGE_ENHANCE, SMOOTH
 from PIL import Image, ImageDraw
-from cv2 import imread, cvtColor, threshold, findContours, COLOR_BGR2GRAY, RETR_TREE, CHAIN_APPROX_SIMPLE
-from cv import CreateImageHeader, SetData, GetMat, IPL_DEPTH_8U
+from cv2 import imread, cvtColor, threshold, findContours, COLOR_BGR2GRAY, RETR_TREE
+from cv import CreateImageHeader, SetData, GetMat, IPL_DEPTH_8U, CV_CHAIN_APPROX_NONE
 from numpy import asarray
 
 
@@ -20,8 +20,8 @@ class EFD(object):
     def get_curve(self):
         im = asarray(GetMat(self.image))
         im = cvtColor(im, COLOR_BGR2GRAY)
-        ret, im = threshold(im, 127,255, 0)
-        contours, hierarchy = findContours(im, RETR_TREE, CHAIN_APPROX_SIMPLE)
+        # ret, im = threshold(im, 127,255, 0)
+        contours, hierarchy = findContours(im, RETR_TREE, CV_CHAIN_APPROX_NONE)
 
         
             
@@ -35,10 +35,12 @@ class EFD(object):
         for i in range(self.m):
             xy_.append(xy[i*step])
         # # Debugging
-        # im_ = Image.new('RGB', (1200,1200), (255,255,255))
+        # im_ = Image.new('RGB', (4000,3000), (255,255,255))
         # pixs = im_.load()
-        # for pos in xy_:
-        #     pixs[pos] = (0,0,0)
+        # draw = ImageDraw.Draw(im_)
+        # for pos in range(len(xy_)):
+        #     draw.line((xy_[pos-1][0],xy_[pos-1][1]+600,xy_[pos][0],xy_[pos][1]+600), fill=128)
+
         # im_.show()
         # # Debugging end
         return xy_
@@ -62,33 +64,33 @@ class EFD(object):
         self.coeffs = coeffs
         return coeffs, ax, ay, bx, by
 
-# ''' Debugging '''
-#     def reconstruct(self):
-#         coeffs, ax, ay, bx, by = self.fourier_coefficients()
-#         x = y = [0]*1000
-#         t = 2*pi/self.m
-#         x = [ax[0]/2.0 + sum([ax[k]*cos((k+1)*t*i) + bx[k]*sin((k+1)*t*i) for k in range(self.ndescriptors)]) for i in range(1000)]
-#         y = [ay[0]/2.0 + sum([ay[k]*cos((k+1)*t*i) + by[k]*sin((k+1)*t*i) for k in range(self.ndescriptors)]) for i in range(1000)]
-#         # for i in range(self.m):
-#         #     x[i] = ax[0]/2.0
-#         #     y[i] = ay[0]/2.0
-            
-#         #     for k in range(len(self.coeffs))[1:]:
-#         #         p = t*k*i
-#         #         x[i] += ax[k]*cos(p) + bx[k]*sin(p)
-#         #         y[i] += ay[k]*cos(p) + by[k]*sin(p)
-#         im = Image.new('RGB',(1500,1500),(255,255,255))
-#         draw = ImageDraw.Draw(im)
-#         pixels = im.load()
-#         for i in range(1000)[:-1]:
-#             xi = x[i] + 650
-#             yi = y[i] + 750
-#             draw.line((x[i-1]+650, y[i-1] + 750, xi, yi), fill=128)
-#         im.show()
 
-# if __name__ == '__main__':
-#     # im = imread(argv[1])
-#     im = Image.open(argv[1])
-#     efd = EFD(im, 150, 1)
-#     # efd.fourier_coefficients()
-#     efd.reconstruct()
+    def reconstruct(self):
+        coeffs, ax, ay, bx, by = self.fourier_coefficients()
+        x = y = [0]*1000
+        t = 2*pi/self.m
+        x = [ax[0]/2.0 + sum([ax[k]*cos((k+1)*t*i) + bx[k]*sin((k+1)*t*i) for k in range(self.ndescriptors)]) for i in range(1000)]
+        y = [ay[0]/2.0 + sum([ay[k]*cos((k+1)*t*i) + by[k]*sin((k+1)*t*i) for k in range(self.ndescriptors)]) for i in range(1000)]
+        # for i in range(self.m):
+        #     x[i] = ax[0]/2.0
+        #     y[i] = ay[0]/2.0
+            
+        #     for k in range(len(self.coeffs))[1:]:
+        #         p = t*k*i
+        #         x[i] += ax[k]*cos(p) + bx[k]*sin(p)
+        #         y[i] += ay[k]*cos(p) + by[k]*sin(p)
+        im = Image.new('RGB',(1500,1500),(255,255,255))
+        draw = ImageDraw.Draw(im)
+        pixels = im.load()
+        for i in range(1000)[1:]:
+            xi = x[i] + 650
+            yi = y[i] + 750
+            draw.line((x[i-1]+650, y[i-1] + 750, xi, yi), fill=128)
+        im.show()
+
+if __name__ == '__main__':
+    # im = imread(argv[1])
+    from plants_libs.threshold import Threshold
+    efd = EFD(Threshold(argv[1]).process(), 300, 1)
+    # efd.fourier_coefficients()
+    efd.reconstruct()
